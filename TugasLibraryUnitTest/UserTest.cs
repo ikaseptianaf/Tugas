84a0d66a01b5c6ae3177e00df603264e0c7a1164
ikaseptianaf <ikaseptianaf@students.amikom.ac.id>
2021-10-28 14:57:49 +0700

    first commit

---
+++ TugasLibraryUnitTest/UserTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TugasLibrary;
+using NUnit.Framework;
+
+namespace TugasLibraryUnitTest
+{
+    [TestFixture]
+    public class UserTest
+    {
+        private User user;
+
+        [SetUp]
+        public void Init()
+        {
+            user = new User();
+        }
+
+        [Test]
+        public void TestValidUser()
+        {
+            var userName = " admin";
+            var password = " admin";
+
+            Assert.IsFalse(user.IsValidUser(userName, password));
+        }
+    }
+}
